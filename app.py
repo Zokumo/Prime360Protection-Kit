fe29e4058c934f7cac25f25542ba6a5a10f3c149
Zokumo <eshanghosh10@gmail.com>
2025-11-08 18:28:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
-# placeholder server
-print('server running')
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Prime360Protection-Kit server is running ✅"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
+
